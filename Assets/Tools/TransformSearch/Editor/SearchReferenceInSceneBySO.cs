84dfc9f0a8b99a7451e0173e63a128f9126b4543
yun.wang <wy1991022219910222@163.com>
2024-07-17 15:40:54 +0800

    补提：兼容2021.1及之前版本

--- Assets/Tools/TransformSearch/Editor/SearchReferenceInSceneBySO.cs
+++ Assets/Tools/TransformSearch/Editor/SearchReferenceInSceneBySO.cs
@@ -11,9 +11,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
-#if UNITY_2021_2_OR_NEWER
 using UnityEditor.SceneManagement;
-#else
+#if !UNITY_2021_2_OR_NEWER
 using UnityEditor.Experimental.SceneManagement;
 #endif
 
