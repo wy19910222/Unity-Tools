ecec890ac2cae2752043c69a56cc6bd8d2a1071f
yun.wang <wy1991022219910222@163.com>
2024-08-11 21:10:51 +0800

    整理代码

--- Assets/Tools/AudioFactory/Editor/AudioClipper.cs
+++ Assets/Tools/AudioFactory/Editor/AudioClipper.cs
@@ -1010,20 +1010,4 @@ namespace WYTools.AudioFactory {
 		}
 		#endregion
 	}
-
-	public class PopupContent : PopupWindowContent {
-		private float Width { get; }
-		private float Height { get; }
-		private Action<Rect> OnDraw { get; }
-
-		public PopupContent(float width, float height, Action<Rect> onDraw) {
-			Width = width;
-			Height = height;
-			OnDraw = onDraw;
-		}
-
-		public override void OnGUI(Rect rect) => OnDraw?.Invoke(rect);
-
-		public override Vector2 GetWindowSize() => new Vector2(Width, Height);
-	}
 }
---
+++ Assets/Tools/AudioFactory/Editor/PopupContent.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-08-08 21:44:43 731
+ * @LastEditor: wangyun
+ * @EditTime: 2024-08-08 21:44:43 735
+ */
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace WYTools.AudioFactory {
+	public class PopupContent : PopupWindowContent {
+		private float Width { get; }
+		private float Height { get; }
+		private Action<Rect> OnGUIAction { get; }
+		public Action OnOpenAction { get; set; }
+		public Action OnCloseAction { get; set; }
+
+		public PopupContent(float width, float height, Action<Rect> onGUIAction) {
+			Width = width;
+			Height = height;
+			OnGUIAction = onGUIAction;
+		}
+
+		public override Vector2 GetWindowSize() => new Vector2(Width, Height);
+
+		public override void OnGUI(Rect rect) => OnGUIAction?.Invoke(rect);
+
+		public override void OnOpen() => OnOpenAction?.Invoke();
+
+		public override void OnClose() => OnCloseAction?.Invoke();
+	}
+}
