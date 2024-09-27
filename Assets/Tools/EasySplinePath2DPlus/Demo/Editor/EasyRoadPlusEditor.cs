a1ba541a9ef58c611fdd2e462f458126bedbbd44
yun.wang <wy1991022219910222@163.com>
2024-09-27 22:17:40 +0800

    EasySplinePath2DPlus：Demo里的FollowPathPlus，朝向逻辑修改；新增EasyRoadPlus；

---
+++ Assets/Tools/EasySplinePath2DPlus/Demo/Editor/EasyRoadPlusEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Editor script, used to update the road in real time.
+/// </summary>
+[CustomEditor(typeof(EasyRoadPlus))]
+public class EasyRoadPlusEditor : Editor
+{
+    EasyRoadPlus creator;
+
+    void OnSceneGUI()
+    {
+        if (creator.liveUpdate && Event.current.type == EventType.Repaint)
+        {
+            creator.UpdateMesh();
+        }
+    }
+
+    void OnEnable()
+    {
+        creator = (EasyRoadPlus)target;
+    }
+}
--- Assets/Tools/EasySplinePath2DPlus/Demo/FollowPathPlus.cs
+++ Assets/Tools/EasySplinePath2DPlus/Demo/FollowPathPlus.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// This is an example of the usage of Easy Spline Path 2D Plus.
 /// In this example we use the function 'GetPointByDistance' to get the position a point in 
-/// the curve corresponding to a distance messured along the curve starting from 
-/// the first Node.
+/// the curve corresponding to a distance measured along the curve starting from the first Node.
 /// </summary>
 public class FollowPathPlus : MonoBehaviour {
 	// A link to the GameObject containing the EasySplinePath2DPlus script (provided by user in the editor)
@@ -19,17 +16,53 @@ public class FollowPathPlus : MonoBehaviour {
 	public bool align = false;
 
 	// Set the position to the curve position at 'dist' distance and calculate the next distance at current speed.
-	protected float dist = 0;
+	public float dist = 0;
 
 	private void Update() {
-		Transform trans = transform;
-		Transform splineTrans = spline2D.transform;
-		trans.position = spline2D.GetPointByDistance(dist, true);
+		transform.position = spline2D.GetPointByDistance(dist, true);
 		dist += speed * Time.deltaTime;
 		if (align) {
-			Vector3 dir = spline2D.GetPointByDistance(dist, true) - transform.position;
-			float angle = Vector3.SignedAngle(splineTrans.right, dir, splineTrans.forward);
-			trans.rotation = splineTrans.rotation * Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.LookAt(spline2D.GetPointByDistance(dist, true), -spline2D.transform.forward);
 		}
 	}
+
+#if UNITY_EDITOR
+	[Header("Editor")]
+	public bool moveOnValidate;
+	public bool alignOnValidate;
+
+	private void OnValidate() {
+		if (spline2D) {
+			spline2D.SetUp();
+			dist = Mathf.Repeat(dist, spline2D.GetLength());
+			if (moveOnValidate) {
+				Move();
+			}
+			if (alignOnValidate) {
+				Align();
+			}
+		}
+	}
+	
+	[ContextMenu("Move")]
+	private void Move() {
+		transform.position = spline2D.GetPointByDistance(dist, true);
+	}
+	
+	[ContextMenu("Align")]
+	private void Align() {
+		float dist1, dist2;
+		float length = spline2D.GetLength();
+		if (dist < length) {
+			dist1 = dist;
+			dist2 = Mathf.Min(dist + 0.00001F, length);
+		} else {
+			dist1 = Mathf.Max(length - 0.00001F, 0);
+			dist2 = length;
+		}
+		Vector3 pos1 = spline2D.GetPointByDistance(dist1, true);
+		Vector3 pos2 = spline2D.GetPointByDistance(dist2, true);
+		transform.LookAt(transform.position + (pos2 - pos1), -spline2D.transform.forward);
+	}
+#endif
 }
