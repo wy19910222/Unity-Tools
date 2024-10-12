ea44486c3c8b14f5686bdee7fd2d2a4dddef9eab
yun.wang <wy1991022219910222@163.com>
2024-10-12 11:24:56 +0800

    漏传了一个文件

---
+++ Assets/Tools/ImageCropping/Editor/PopupContent.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * @Author: wangyun
+ * @CreateTime: 2024-08-08 21:44:43 731
+ * @LastEditor: wangyun
+ * @EditTime: 2024-08-08 21:44:43 735
+ */
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace WYTools.ImageCropping {
+	public class PopupContent : PopupWindowContent {
+		public float Width { get; set; }
+		public float Height { get; set; }
+		public Action<Rect> OnGUIAction { get; set; }
+		public Action OnOpenAction { get; set; }
+		public Action OnCloseAction { get; set; }
+
+		public override Vector2 GetWindowSize() => new Vector2(Width, Height);
+
+		public override void OnGUI(Rect rect) => OnGUIAction?.Invoke(rect);
+
+		public override void OnOpen() => OnOpenAction?.Invoke();
+
+		public override void OnClose() => OnCloseAction?.Invoke();
+	}
+}
